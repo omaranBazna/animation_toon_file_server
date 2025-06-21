56c09d9fab1cc5472c2cdca541a76b5cd18d9f75
Horizon-hub <omran4d@gmail.com>
2025-06-21 11:22:41 -0400

    change host

--- index.py
+++ index.py
@@ -422,4 +422,4 @@ async def merged_videos_list():
     return HTMLResponse(content=html)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.1.2", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
